6d88915a596f03f6ab7fda7db801f61de966e53f
karsh <utkarsh@cyberfear.com>
2024-01-09 14:51:06 +1100

    ✨ basic structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
+    let mut counter: u32 = 1;
+    let mut run: bool = true;
 
+    // initialise all components
+    
+    while run {
+        // update
+
+        // actions
+
+        // counter update
+
+        // if program must keep running
+    }
+
+    // free all components
 }
