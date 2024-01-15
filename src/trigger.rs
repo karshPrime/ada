dd65f6cf8f13f45c99547de2a8f06800fc48deb9
karsh <utkarsh@cyberfear.com>
2024-01-15 11:13:37 +1100

    ✨ non-conditional counter update

--- src/trigger.rs
+++ src/trigger.rs
@@ -16,10 +16,7 @@ pub fn keep_running() -> bool {
 }
 
 //
-pub fn update_counter(counter: &mut u32) -> () {
-    if *counter == u32::MAX {
-        *counter = 1;
-    } else {
-        *counter += 1;
-    }
+pub fn update_counter(counter: &mut u32)  {
+    *counter = 1 + (*counter * (*counter == u32::MAX) as u32);
 }
+
