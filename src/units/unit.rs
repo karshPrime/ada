6c58e0cd287bd32fecfccde93b39ed5450cc61b4
karsh <utkarsh@cyberfear.com>
2024-01-18 11:44:42 +1100

    ✨ pace
    Universal pace constants; would make it easier to define blink/beep and other repetitive tasks' speed

--- src/units/unit.rs
+++ src/units/unit.rs
@@ -4,3 +4,21 @@ pub trait Component {
     fn init(chip: &Chip) -> Self;
     fn update(&mut self, counter: &u32);
 }
+
+#[derive(Debug, Copy, Clone)]
+pub enum Pace {
+    Fast,
+    Medium,
+    Slow,
+    Still,
+}
+
+pub fn pace_value(pace: Pace) -> u32 {
+    match pace {
+        Pace::Fast => 100,
+        Pace::Medium => 75,
+        Pace::Slow => 50,
+        Pace::Still => 0,
+    }
+}
+
