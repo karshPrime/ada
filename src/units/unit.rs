1e514b12f0410c9e131dafcb0f26904466d3de2c
karsh <utkarsh@cyberfear.com>
2024-01-20 13:45:17 +1100

    ✨ more speed options
    higher the value, slower the speed;

--- src/units/unit.rs
+++ src/units/unit.rs
@@ -7,18 +7,22 @@ pub trait Component {
 
 #[derive(Debug, Copy, Clone)]
 pub enum Pace {
+    Rapid,
     Fast,
-    Medium,
+    Moderate,
     Slow,
+    Torpid,
     Still,
 }
 
 pub fn pace_value(pace: Pace) -> u32 {
     match pace {
-        Pace::Fast => 100,
-        Pace::Medium => 75,
-        Pace::Slow => 50,
-        Pace::Still => 0,
+        Pace::Rapid    => 500,
+        Pace::Fast     => 1000,
+        Pace::Moderate => 2500,
+        Pace::Slow     => 5000,
+        Pace::Torpid   => 50000,
+        Pace::Still    => 0,
     }
 }
 
